01b3cdb387fc3c36992f4266a0dbb32abeaa0ecc
Gabriel Mattos Langeloh <gmlangeloh@inf.ufrgs.br>
2024-01-22 12:36:50 -0300

    Automatic testing with Aqua.jl

--- test/Aqua.jl
+++ test/Aqua.jl
@@ -1,4 +1,6 @@
 using Aqua
 
-Aqua.test_all(IPGBs, ambiguities=false)
-Aqua.test_ambiguities(IPGBs)
+@testset "Aqua.jl" begin
+    Aqua.test_all(IPGBs, ambiguities=false)
+    Aqua.test_ambiguities(IPGBs)
+end
